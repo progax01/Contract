f2ab5b7ece9d3a5bc42c108fd0d05f478ec382c8
progax01 <anurag35412@gmail.com>
2023-09-02 09:55:08 +0530

    Initial commit

---
+++ Manager.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Manager {
+    string usertype1;
+    string usertype2;
+    address public owner;
+
+    struct User {
+        string username;
+        string usertype;
+        bool registered;
+    }
+
+    mapping(address => User) public users;
+
+    constructor(string memory _type1, string memory _type2) {
+        usertype1 = _type1;
+        usertype2 = _type2;
+        owner = msg.sender;
+    }
+
+    function user_register(string memory usertype, string memory username)
+        public
+    {
+        require(
+            keccak256(bytes(usertype)) == keccak256(bytes(usertype1)) ||
+                keccak256(bytes(usertype)) == keccak256(bytes(usertype2)),
+            "Invalid user type"
+        );
+      
+
+        users[msg.sender] = User(username, usertype, true);
+    }
+
+   function getUserType(address _address) public view returns (string memory) {
+    require(users[_address].registered, "User is not registered");
+
+ 
+    
+
+    return users[_address].usertype;
+    }
+
+    function isUserRegistered(address _address) public view returns (bool) {
+        return users[_address].registered;
+    }
+}
